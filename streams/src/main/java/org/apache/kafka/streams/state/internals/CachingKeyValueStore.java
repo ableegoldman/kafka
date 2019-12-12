791f7858d5d15557bb3f96c0bfc11ff44f8a277a
ableegoldman <sophie@confluent.io>
2019-12-11 18:00:09 -0800

    insert with KVStore#putAll

--- streams/src/main/java/org/apache/kafka/streams/state/internals/CachingKeyValueStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/CachingKeyValueStore.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kafka.streams.state.internals;
 
+import java.util.ArrayList;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.processor.ProcessorContext;
@@ -67,40 +68,45 @@ public class CachingKeyValueStore
 
         this.cache = this.context.getCache();
         this.cacheName = ThreadCache.nameSpaceFromTaskIdAndStore(context.taskId().toString(), name());
-        cache.addDirtyEntryFlushListener(cacheName, entries -> {
-            for (final ThreadCache.DirtyEntry entry : entries) {
-                putAndMaybeForward(entry, (InternalProcessorContext) context);
-            }
-        });
+        cache.addDirtyEntryFlushListener(cacheName, entries ->
+                putAndMaybeForward(entries, (InternalProcessorContext) context));
     }
 
-    private void putAndMaybeForward(final ThreadCache.DirtyEntry entry,
+    private void putAndMaybeForward(final List<ThreadCache.DirtyEntry> entries,
                                     final InternalProcessorContext context) {
+
+        final List<KeyValue<Bytes, byte[]>> records = new ArrayList<>();
         if (flushListener != null) {
-            final byte[] rawNewValue = entry.newValue();
-            final byte[] rawOldValue = rawNewValue == null || sendOldValues ? wrapped().get(entry.key()) : null;
+            for (final ThreadCache.DirtyEntry entry : entries) {
 
-            // this is an optimization: if this key did not exist in underlying store and also not in the cache,
-            // we can skip flushing to downstream as well as writing to underlying store
-            if (rawNewValue != null || rawOldValue != null) {
-                // we need to get the old values if needed, and then put to store, and then flush
-                wrapped().put(entry.key(), entry.newValue());
+                final byte[] rawNewValue = entry.newValue();
+                final byte[] rawOldValue = rawNewValue == null || sendOldValues ? wrapped().get(entry.key()) : null;
 
-                final ProcessorRecordContext current = context.recordContext();
-                context.setRecordContext(entry.entry().context());
-                try {
-                    flushListener.apply(
-                        entry.key().get(),
-                        rawNewValue,
-                        sendOldValues ? rawOldValue : null,
-                        entry.entry().context().timestamp());
-                } finally {
-                    context.setRecordContext(current);
+                // this is an optimization: if this key did not exist in underlying store and also not in the cache,
+                // we can skip flushing to downstream as well as writing to underlying store
+                if (rawNewValue != null || rawOldValue != null) {
+                    // we need to get the old values if needed, and then put to store, and then flush
+                    records.add(new KeyValue<>(entry.key(), entry.newValue()));
+
+                    final ProcessorRecordContext current = context.recordContext();
+                    context.setRecordContext(entry.entry().context());
+                    try {
+                        flushListener.apply(
+                            entry.key().get(),
+                            rawNewValue,
+                            sendOldValues ? rawOldValue : null,
+                            entry.entry().context().timestamp());
+                    } finally {
+                        context.setRecordContext(current);
+                    }
                 }
             }
         } else {
-            wrapped().put(entry.key(), entry.newValue());
+            for (final ThreadCache.DirtyEntry entry : entries) {
+                records.add(new KeyValue<>(entry.key(), entry.newValue()));
+            }
         }
+        wrapped().putAll(records);
     }
 
     @Override
