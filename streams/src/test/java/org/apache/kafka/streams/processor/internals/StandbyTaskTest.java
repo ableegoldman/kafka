9bfbff7147f8812966e7b491fc9955d5a4512bff
A. Sophie Blee-Goldman <sophie@confluent.io>
2021-10-09 03:38:14 -0500

    test fixes and cache 0 case

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/StandbyTaskTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/StandbyTaskTest.java
@@ -578,7 +578,7 @@ public class StandbyTaskTest {
     }
 
     @Test
-    public void shouldCLearTaskTimeout() {
+    public void shouldClearTaskTimeout() {
         EasyMock.replay(stateManager);
 
         task = createStandbyTask();
@@ -608,7 +608,7 @@ public class StandbyTaskTest {
             taskId,
             Collections.singleton(partition),
             topology,
-            new TopologyConfig(null, config, new Properties()).getTaskConfig(),
+            new TopologyConfig(config).getTaskConfig(),
             streamsMetrics,
             stateManager,
             stateDirectory,
