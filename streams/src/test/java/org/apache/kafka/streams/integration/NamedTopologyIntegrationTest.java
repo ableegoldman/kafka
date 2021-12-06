df628bcbebf4dd8d09f612a39cd7eae37e24c105
Tolga Dur <mail@tolgadur.com>
2021-12-06 17:17:07 +0000

    queryMetadataForKey()

--- streams/src/test/java/org/apache/kafka/streams/integration/NamedTopologyIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/NamedTopologyIntegrationTest.java
@@ -348,11 +348,6 @@ public class NamedTopologyIntegrationTest {
         final ReadOnlyKeyValueStore<String, Long> store =
             streams.store(NamedTopologyStoreQueryParameters.fromNamedTopologyAndStoreNameAndType("topology-1", "store", QueryableStoreTypes.keyValueStore()));
         assertThat(store.get("A"), equalTo(2L));
-
-//        Collection<StreamsMetadata> test1 = streams.streamsMetadataForStore("store");
-        Collection<StreamsMetadata> streamsMetadata = streams.streamsMetadataForStore("store", "topology-1");
-
-//        System.out.println(test1);
     }
 
     @Test
@@ -391,6 +386,9 @@ public class NamedTopologyIntegrationTest {
 
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3), equalTo(COUNT_OUTPUT_DATA));
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 3), equalTo(COUNT_OUTPUT_DATA));
+
+        Collection<StreamsMetadata> test1 = streams.streamsMetadataForStore("store");
+        Collection<StreamsMetadata> streamsMetadata = streams.streamsMetadataForStore("store", "topology-1");
 
         // TODO KAFKA-12648: need to make sure that both instances actually did some of this processing of topology-2,
         //  ie that both joined the group after the new topology was added and then successfully processed records from it
